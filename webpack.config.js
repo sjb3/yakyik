cd06870011472fca63123fa7565e4a7ac53bf3ad
Sung J Byun <sungjbyun@D-173-250-158-241.dhcp4.washington.edu>
2016-12-16 19:03:49 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: {
+		app: './src/app.js'
+	},
+	output: {
+		filename: 'public/build/bundle.js',
+		sourceMapFilename: 'public/build/bundle.map'
+	},
+	devtool: '#source-map',
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				exclude: /(node_modules)/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			}
+		]
+	}
+}
