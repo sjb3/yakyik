d5935bd89381d31206c80eed8af702663b864e3b
Sung J Byun <sungjbyun@D-173-250-237-108.dhcp4.washington.edu>
2016-12-19 15:51:00 -0800

    still zone/ not working

--- routes/api.js
+++ routes/api.js
@@ -8,27 +8,39 @@ const zoneController = require('../controllers/zone-controller');
 const controllers = require('../controllers');
 // const zoneRouter = module.exports = new router;
 
-router.get('/:resource/:id', function(req, res, next){
+router.get('/:resource/:id', (req, res, next) => {
   debug('get /zone/:id');
 
   var resource = req.params.resource;
   var id = req.params.id;
 
-  if(resource =='zone'){
-    zoneController.fetchZoneById(id, function(err, results){
-      if(err){
-        res.json({
-          confirmation: 'failed',
-          message : 'Not Found'
-        })
-      }
-      return res.json ({ confirmation: 'success', results: results })
-    });
+  // if(resource == 'zone'){
+  //   zoneController.fetchZoneById(id, (err, result) => {
+  //     if(err){
+  //       res.json({
+  //         confirmation: 'failed',
+  //         message : 'Not Found'
+  //       })
+  //     }
+  //     return res.json ({ confirmation: 'success', result: result })
+  //   });
+  // };
+  
+  if( resource == 'zone'){
+    zoneController.fetchZoneById(id)
+    .then( zone => res.json(zone))
+    .catch( next );
   };
+  return res.json ({ confirmation: 'success', result: result })
 });
 
-  // zoneController.fetchZoneById(id)
-  // .then(zone => res.json(zone))
-  // .catch(next);
-// });
+router.post('/:resource', (req, res, next) => {
+  debug('post /createzone/')
+  zoneController.createZone(req.body)
+  .then((zone) => {
+    res.json(zone)
+  })
+  .catch(next);
+});
+
 module.exports = router;
