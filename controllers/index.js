24b19ec7505b3b6cb241e1374809765c545b4649
Sung J Byun <sungjbyun@D-173-250-237-108.dhcp4.washington.edu>
2016-12-19 20:41:28 -0800

    change back to ES5

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const zoneController = require('./zone-controller');
+const commentController = require('./comment-controller');
+
+module.exports = {
+  comment: commentController,
+  zone: zoneController
+};
